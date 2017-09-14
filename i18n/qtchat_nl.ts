a53465754e0adb758197fe79f2cbf16a28a093fa
Heimen Stoffels <vistausss@outlook.com>
2017-09-14 20:22:36 +0200

    Added Dutch translation

---
+++ i18n/qtchat_nl.ts
@@ -0,0 +1,291 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>ChatRoom</name>
+    <message>
+        <location filename="../src/chatroom.ui" line="18"/>
+        <source>ChatRoom</source>
+        <translation>Chatroom</translation>
+    </message>
+    <message>
+        <location filename="../src/chatroom.ui" line="39"/>
+        <source>New message</source>
+        <translation>Nieuw bericht</translation>
+    </message>
+    <message>
+        <location filename="../src/chatroom.ui" line="194"/>
+        <source>Send</source>
+        <translation>Versturen</translation>
+    </message>
+    <message>
+        <location filename="../src/chatroom.ui" line="216"/>
+        <source>online</source>
+        <translation>beschikbaar</translation>
+    </message>
+    <message>
+        <location filename="../src/chatroom.ui" line="225"/>
+        <source>away</source>
+        <translation>afwezig</translation>
+    </message>
+    <message>
+        <location filename="../src/chatroom.ui" line="234"/>
+        <source>busy</source>
+        <translation>bezig</translation>
+    </message>
+    <message>
+        <location filename="../src/chatroom.ui" line="243"/>
+        <source>offline</source>
+        <translation>offline</translation>
+    </message>
+    <message>
+        <location filename="../src/chatroom.ui" line="252"/>
+        <source>invisible</source>
+        <translation>onzichtbaar</translation>
+    </message>
+    <message>
+        <location filename="../src/chatroom.ui" line="264"/>
+        <source>Users</source>
+        <translation>Gebruikers</translation>
+    </message>
+    <message>
+        <location filename="../src/chatroom.ui" line="290"/>
+        <source>Refresh</source>
+        <translation>Verversen</translation>
+    </message>
+    <message>
+        <location filename="../src/chatroom.cpp" line="69"/>
+        <source>Global chat</source>
+        <translation>Algemene chat</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.ui" line="14"/>
+        <source>QtChat</source>
+        <translation>QtChat</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="40"/>
+        <source>&amp;File</source>
+        <translation>&amp;Bestand</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="48"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Hulp</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="63"/>
+        <source>&amp;Exit</source>
+        <translation>&amp;Afsluiten</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="72"/>
+        <source>&amp;About QtChat</source>
+        <translation>&amp;Over QtChat</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="81"/>
+        <source>&amp;About Qt</source>
+        <translation>&amp;Over Qt</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="90"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Instellingen</translation>
+    </message>
+</context>
+<context>
+    <name>ReceiveFileDialog</name>
+    <message>
+        <location filename="../src/receivefiledialog.ui" line="14"/>
+        <source>Receiving file</source>
+        <translation>Bezig met ontvangen van bestand</translation>
+    </message>
+    <message>
+        <location filename="../src/receivefiledialog.ui" line="22"/>
+        <source>File:</source>
+        <translation>Bestand:</translation>
+    </message>
+    <message>
+        <location filename="../src/receivefiledialog.ui" line="60"/>
+        <source>Cancel</source>
+        <translation>Annuleren</translation>
+    </message>
+    <message>
+        <location filename="../src/receivefiledialog.ui" line="71"/>
+        <source>Ok</source>
+        <translation>Oké</translation>
+    </message>
+    <message>
+        <location filename="../src/receivefiledialog.cpp" line="36"/>
+        <source>%1 wants to send you a file</source>
+        <translation>%1 wil u een bestand sturen</translation>
+    </message>
+    <message>
+        <location filename="../src/receivefiledialog.cpp" line="36"/>
+        <source>Do you accept this file transfer ?</source>
+        <translation>Wilt u deze bestandsoverdracht accepteren?</translation>
+    </message>
+    <message>
+        <location filename="../src/receivefiledialog.cpp" line="80"/>
+        <location filename="../src/receivefiledialog.cpp" line="94"/>
+        <source>Error</source>
+        <translation>Fout</translation>
+    </message>
+    <message>
+        <location filename="../src/receivefiledialog.cpp" line="80"/>
+        <source>Could not start file transfer: sender has disconnected!</source>
+        <translation>Overdracht kan niet worden gestart: verstuurder heeft de verbinding verbroken!</translation>
+    </message>
+    <message>
+        <location filename="../src/receivefiledialog.cpp" line="84"/>
+        <source>Save received file</source>
+        <translation>Ontvangen bestand opslaan</translation>
+    </message>
+    <message>
+        <location filename="../src/receivefiledialog.cpp" line="94"/>
+        <source>Could not open file for writing! Aborting file transfer.</source>
+        <translation>Het bestand kan niet worden geopend voor schrijven! Overdracht wordt afgebroken.</translation>
+    </message>
+</context>
+<context>
+    <name>SendFileDialog</name>
+    <message>
+        <location filename="../src/sendfiledialog.ui" line="14"/>
+        <source>Sending file</source>
+        <translation>Bezig met versturen van bestand</translation>
+    </message>
+    <message>
+        <location filename="../src/sendfiledialog.ui" line="22"/>
+        <source>File:</source>
+        <translation>Bestand:</translation>
+    </message>
+    <message>
+        <location filename="../src/sendfiledialog.ui" line="60"/>
+        <source>Cancel</source>
+        <translation>Annuleren</translation>
+    </message>
+    <message>
+        <location filename="../src/sendfiledialog.ui" line="71"/>
+        <source>Ok</source>
+        <translation>Oké</translation>
+    </message>
+    <message>
+        <location filename="../src/sendfiledialog.cpp" line="30"/>
+        <source>Could not read file</source>
+        <translation>Bestand kan niet worden gelezen</translation>
+    </message>
+    <message>
+        <location filename="../src/sendfiledialog.cpp" line="30"/>
+        <source>Could not open file &apos;%1&apos; for reading!</source>
+        <translation>Bestand &apos;%1&apos; kan niet worden geopend voor lezen!</translation>
+    </message>
+    <message>
+        <location filename="../src/sendfiledialog.cpp" line="43"/>
+        <source>Could not send file</source>
+        <translation>Bestand kan niet worden verstuurd</translation>
+    </message>
+    <message>
+        <location filename="../src/sendfiledialog.cpp" line="43"/>
+        <source>User is not connected!</source>
+        <translation>Gebruiker is niet verbonden!</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../src/settings.ui" line="14"/>
+        <source>Settings</source>
+        <translation>Instellingen</translation>
+    </message>
+    <message>
+        <location filename="../src/settings.ui" line="24"/>
+        <source>Main</source>
+        <translation>Algemeen</translation>
+    </message>
+    <message>
+        <location filename="../src/settings.ui" line="30"/>
+        <source>Execute command on incoming messages:</source>
+        <translation>Opdracht uitvoeren bij inkomende berichten:</translation>
+    </message>
+    <message>
+        <location filename="../src/settings.ui" line="58"/>
+        <source>User</source>
+        <translation>Gebruiker</translation>
+    </message>
+    <message>
+        <location filename="../src/settings.ui" line="64"/>
+        <source>Nick name</source>
+        <translation>Bijnaam</translation>
+    </message>
+    <message>
+        <location filename="../src/settings.ui" line="93"/>
+        <source>OK</source>
+        <translation>Oké</translation>
+    </message>
+    <message>
+        <location filename="../src/settings.ui" line="104"/>
+        <source>Cancel</source>
+        <translation>Annuleren</translation>
+    </message>
+</context>
+<context>
+    <name>UserManager</name>
+    <message>
+        <location filename="../src/usermanager.cpp" line="28"/>
+        <source>QtChat must stop</source>
+        <translation>QtChat moet worden gestopt</translation>
+    </message>
+    <message>
+        <location filename="../src/usermanager.cpp" line="28"/>
+        <source>QtChat server could not start!</source>
+        <translation>De QtChat-server kan niet worden gestart!</translation>
+    </message>
+</context>
+<context>
+    <name>UserWidget</name>
+    <message>
+        <location filename="../src/userwidget.cpp" line="36"/>
+        <source>Send file</source>
+        <translation>Bestand versturen</translation>
+    </message>
+    <message>
+        <location filename="../src/userwidget.cpp" line="40"/>
+        <source>Private chat</source>
+        <translation>Privéchat</translation>
+    </message>
+    <message>
+        <location filename="../src/userwidget.cpp" line="51"/>
+        <source>Send file to </source>
+        <translation>Bestand versturen aan </translation>
+    </message>
+    <message>
+        <location filename="../src/userwidget.cpp" line="72"/>
+        <source>away</source>
+        <translation>afwezig</translation>
+    </message>
+    <message>
+        <location filename="../src/userwidget.cpp" line="77"/>
+        <source>busy</source>
+        <translation>bezig</translation>
+    </message>
+    <message>
+        <location filename="../src/userwidget.cpp" line="82"/>
+        <source>offline</source>
+        <translation>offline</translation>
+    </message>
+    <message>
+        <location filename="../src/userwidget.cpp" line="87"/>
+        <source>invisible</source>
+        <translation>onzichtbaar</translation>
+    </message>
+    <message>
+        <location filename="../src/userwidget.cpp" line="92"/>
+        <source>online</source>
+        <translation>beschikbaar</translation>
+    </message>
+</context>
+</TS>
